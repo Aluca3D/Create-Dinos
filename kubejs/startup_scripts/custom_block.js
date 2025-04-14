42c909d23c4107c5dd1b0402c985392a2ad02fb1
luca2 <luca2006.mu@gmail.com>
2025-04-14 21:07:55 +0200

    changed nh_sticker from Item to block

---
+++ kubejs/startup_scripts/custom_block.js
@@ -0,0 +1,13 @@
+console.info('Loaded Custom Blocks')
+StartupEvents.registry('block', event => {
+  event.create('nh_sticker', 'cardinal')
+    .displayName('Nett Hier Sticker')
+    .noCollision()
+    .notSolid()
+    .renderType("cutout")
+    .fullBlock(false)
+    .soundType('lily_pad')
+    .hardness(0.2)
+    .model('cad:blocks/nh_sticker')
+    .property(BlockProperties.WATERLOGGED)
+})
